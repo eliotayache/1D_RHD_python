602027b405d635c2648e12831a38ed338a46ab76
Eliot Ayache <eliot.ayache@gmail.com>
2020-01-14 08:14:41 +0000

    [ADDED] run_ST.py: runs shock tube simulations using package RHD

---
+++ bin/run_ST.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Author: Eliot Ayache
+# @Date:   2020-01-14 08:01:05
+# @Last Modified by:   Eliot Ayache
+# @Last Modified time: 2020-01-14 08:13:51
+
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+    # Adding parent to Pythonpath
+
+from RHD import *
+
+
+
+
+if __name__ == '__main__':
+
+    setup = Setup_ST(rhoL=1.,vL=.1,pL=1.,rhoR=0.1,vR=0.,pR=0.1)
+    solver = Solver_HLL(cfl=0.2)        
+    hydro = Hydro(setup=setup, solver=solver)
+    hydro.run()
+
